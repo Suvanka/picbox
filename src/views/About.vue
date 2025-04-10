cf3ed1428997b76dd74f827fa7c548fffe968651
Suvanka <86843315+Suvanka@users.noreply.github.com>
2025-04-10 21:07:23 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,15 +3,12 @@
   <div class="about-wrapper">
     <div class="title-1">关于</div>
     <div class="content">
-      NIS3366 项目管理与软件设计<br />
-      小作业：图片上传管理工具PicBox<br />
+      图片上传管理工具PicBox<br />
       Github仓库地址：<a
         target="_blank" href="https://github.com/Suvanka/picbox"
       >https://github.com/Suvanka/picbox
       </a><br />
       由于Github在国内访问速度较慢，必要时请使用代理<br /><br />
-      作者：刘凡嘉<br />
-      学号：520021910751
     </div>
   </div>
 </template>
